8521392f15c21727706d7d1c1bc735f7a5d9aa1b
Victor Hugo Fortman <hugovicfortman@gmail.com>
2017-05-09 08:50:07 +0200

    Second Tests and Iterations on Optimus - testing 5 and beyond

--- app/optimus.js
+++ app/optimus.js
@@ -4,39 +4,43 @@ module.exports = {
 
  /* Find all prime numbers from 0 to input limit... */
  getPrimes: (upperArrayLimit) => {
+   // the logic goes here...
    //No even number can be prime except 2...
    //So our suspects are all looking odd today... o_O
    let primeArray = [], countLimit, outputPrimes = [], primesFound = 0;
    //if number is greater than or equal to 3, then 1,2,3 are known
    //offenders and must be brought to the lineup...
-   for(let i = 0; (i < upperArrayLimit && i < 4); i++)
+   for(let i = 0; (i < upperArrayLimit && i < 3); i++)
    {
       outputPrimes[i] = i+1;
-   }/*
-   // your logic goes here
-   for(let i = 3; i<upperArrayLimit; i+=2)
+      primesFound++;
+   }
+   //the rest of the odd numbers from 5 are tested for and included....
+   //adapting seive of somedude...
+   for(let i = 5; i<= upperArrayLimit; i++)
    {
       primeArray[i] = true;
    }
    countLimit = Math.sqrt(upperArrayLimit);
-   for(let i = 2; i < upperArrayLimit; i++)
+
+   for(let i = 5; i < countLimit; i++)
    {
       if(primeArray[i] === true)
       {
-         for(let j = i*i; j< value; j+= i)
+         for(let j = i*i; j< upperArrayLimit; j += i)
          {
-            primes[j] = false;
+            primeArray[j] = false;
          }
       }
    }
-   for(let i= 2; i< upperArrayLimit; i++)
+   for(let i= 5; i<= upperArrayLimit; i++)
    {
       if(primeArray[i] === true)
       {
-         outputPrimes[primesFound] = primeArray[i];
+         outputPrimes[primesFound] = i;
          primesFound++;
       }
-   }*/
+   }
    return outputPrimes;
  }
 }
--- tests/tests.js
+++ tests/tests.js
@@ -9,4 +9,13 @@ describe("When optimus getPrimes function is called ", function() {
   it("should return [1,2] for 2", function() {
     expect(myApp.getPrimes(2)).toEqual([1,2]);
   })
+  it("should return [1,2,3] for 3", function() {
+    expect(myApp.getPrimes(3)).toEqual([1,2,3]);
+  })
+  it("should return [1,2,3] for 4", function() {
+    expect(myApp.getPrimes(4)).toEqual([1,2,3]);
+  })
+  it("should return [1,2,3,5] for 5", function() {
+    expect(myApp.getPrimes(5)).toEqual([1,2,3,5]);
+  })
 })
