71b4272af896d6f7a8fafdbd8dc149b59bd3b6b3
Victor Hugo Fortman <hugovicfortman@gmail.com>
2017-05-09 08:04:59 +0200

    First Test and Iterations on Optimus

--- app/optimus.js
+++ app/optimus.js
@@ -2,10 +2,41 @@
 
 module.exports = {
 
- /* Find the average of three integers */
- getPrimes: (upperLimit) => {
-   let primeArray = [];
+ /* Find all prime numbers from 0 to input limit... */
+ getPrimes: (upperArrayLimit) => {
+   //No even number can be prime except 2...
+   //So our suspects are all looking odd today... o_O
+   let primeArray = [], countLimit, outputPrimes = [], primesFound = 0;
+   //if number is greater than or equal to 3, then 1,2,3 are known
+   //offenders and must be brought to the lineup...
+   for(let i = 0; (i < upperArrayLimit && i < 4); i++)
+   {
+      outputPrimes[i] = i+1;
+   }/*
    // your logic goes here
-   return primeArray;
+   for(let i = 3; i<upperArrayLimit; i+=2)
+   {
+      primeArray[i] = true;
+   }
+   countLimit = Math.sqrt(upperArrayLimit);
+   for(let i = 2; i < upperArrayLimit; i++)
+   {
+      if(primeArray[i] === true)
+      {
+         for(let j = i*i; j< value; j+= i)
+         {
+            primes[j] = false;
+         }
+      }
+   }
+   for(let i= 2; i< upperArrayLimit; i++)
+   {
+      if(primeArray[i] === true)
+      {
+         outputPrimes[primesFound] = primeArray[i];
+         primesFound++;
+      }
+   }*/
+   return outputPrimes;
  }
 }
--- tests/tests.js
+++ tests/tests.js
@@ -6,7 +6,7 @@ const assert = require('chai').assert;
 const myApp = require('../app/optimus.js');
 
 describe("When optimus getPrimes function is called ", function() {
-  it("should return [2] for 2", function() {
-    expect(myApp.getPrimes(2)).toEqual(2);
+  it("should return [1,2] for 2", function() {
+    expect(myApp.getPrimes(2)).toEqual([1,2]);
   })
 })
